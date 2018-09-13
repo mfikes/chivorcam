4c82239fc2b8a99b698e845ae8f0871526590376
Mike Fikes <mike@fikesfarm.com>
2018-09-12 20:05:49 -0400

    0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chivorcam "0.2.0"
+(defproject chivorcam "0.3.0"
   :description "Macros directly in ClojureScript"
   :url "https://github.com/mfikes/chivorcam"
   :dependencies [[org.clojure/clojurescript "1.10.339"]])
